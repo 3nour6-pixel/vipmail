4733b8ef67653b22fe1d20bea44dcc7b5b73c289
3nour6-pixel <3nour6@gmail.com>
2025-10-29 04:11:41 +0300

    Update apiv1.php

--- api/apiv1.php
+++ api/apiv1.php
@@ -9,10 +9,10 @@
 require_once __DIR__ . '/config.php';
 
 // Enable error reporting for debugging (disable in production)
-error_reporting(E_ALL);
+error_reporting(0);
 ini_set('display_errors', 0);
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/error.log');
+ini_set('log_errors', 0);
+
 
 // Set headers
 header('Content-Type: application/json');
@@ -331,6 +331,7 @@ try {
     sendResponse(false, 'An unexpected error occurred. Please try again later.');
 }
 ?>
+
 
 
 
